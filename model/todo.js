f90a35086d426351784221d9dde218eb05f7eea5
Myshheo <myshheo@gmail.com>
2023-11-09 03:20:26 +0900

    todo.js 파일 생성

---
+++ model/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    value: String, //할 일을 확인하는 칼럼
+    doneAt: Date, // 할 일이 언제 완료되었는지,
+    order: Number, //몇번 째 할 일 인지
+});
+// 데이터 조회시 생성되는 가상의 칼럼으로 반환.
+TodoSchema.virtual("todoId").get(()=>this._id.toHexString());
+TodoSchema.set("toJSON", {virtuals: true});
+
+module.exports = mongoose.model("Todo", TodoSchema);
