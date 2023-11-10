480668e63ce375195179d0684b1a6c657cec0023
Myshheo <myshheo@gmail.com>
2023-11-10 19:18:50 +0900

    할 일 조회, 수정

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -24,9 +24,33 @@ router.post('/todos', async(req,res) => {
 
 // 할 일 조회 api
 router.get('/todos', async (req,res)=>{
-    const todoList = await Todo.find({}).sort({order:-1});
-    res.status(200).json(todoList)
-    
+    const todos = await Todo.find().sort("-order").exec();
+    res.send({todos})
+});
+
+// 할 일 순서 변경하는 api
+router.patch('/todos/:todoId', async(req,res)=>{
+    const {todoId} = req.params;
+    const {order} = req.body;
+    // 1. todoId에 해당하는 할 일 이 있는가?
+    // 1-1. 해당하는 할 일이 없다면 에러 출력.
+    const currentTodo = await Todo.findById(todoId);
+    if (!currentTodo) {
+        return res.status(400).json({"errorMessage":"존재하지 않는 할 일 입니다."});
+    };
+    // 
+    if (order){
+        const targetTodo = await Todo.findOne({order}).exec();
+        if (targetTodo){
+            targetTodo.order = currentTodo.order;
+            // database 상에 변경된 데이터를 저장.
+            await targetTodo.save();
+        }
+        currentTodo.order = order;
+        await currentTodo.save();
+    }
+
+    res.status(200).json(req.params)
 })
 
 
